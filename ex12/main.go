4b3efff7e7337007269efe60903b6250f37ea0ab
suzibill <suzibill@student.21-school.ru>
2023-03-12 17:58:56 +0300

    ex12-14

--- ex12/main.go
+++ ex12/main.go
@@ -1,5 +1,16 @@
 package main
 
-  func main() {
+import "fmt"
 
-  }
+type void struct{}
+
+func main() {
+	a := []string{"aboba", "abeba", "aboba", "abeba", "abeba"}
+	set := make(map[string]void)
+	var member void
+
+	for _, v := range a {
+		set[v] = member
+	}
+	fmt.Println(set)
+}
