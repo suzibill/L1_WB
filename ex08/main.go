67bcfdcc81ea3c969b4e494642252122533ce9b5
suzibill <suzibill@student.21-school.ru>
2023-03-12 16:29:55 +0300

    ex8-9

--- ex08/main.go
+++ ex08/main.go
@@ -1,5 +1,27 @@
 package main
 
-  func main() {
+import "fmt"
 
-  }
+func main() {
+	var n int64 = 12
+	fmt.Println(n)
+	n = reverseBit(n, 0, 0)
+	n = reverseBit(n, 1, 0)
+	n = reverseBit(n, 2, 0)
+	fmt.Println(n)
+	n = reverseBit(n, 0, 1)
+	n = reverseBit(n, 1, 0)
+	n = reverseBit(n, 2, 1)
+	fmt.Println(n)
+	n = reverseBit(n, 1, 1)
+	fmt.Println(n)
+}
+
+func reverseBit(n int64, bit uint, status int) int64 {
+	if status == 1 {
+		n |= 1 << (bit % 64)
+	} else if status == 0 {
+		n &^= 1 << (bit % 64)
+	}
+	return n
+}
