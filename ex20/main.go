ad24cb84203ffd238fda405cf62cf1154dc94ed7
suzibill <suzibill@student.21-school.ru>
2023-03-13 22:15:41 +0300

    ex20

--- ex20/main.go
+++ ex20/main.go
@@ -1,5 +1,25 @@
 package main
 
-  func main() {
+import (
+	"fmt"
+	"strings"
+)
 
-  }
+func main() {
+	s := "привет 😀 hello"
+	s = reverseWords(s)
+	fmt.Println(s)
+}
+
+func reverseWords(s string) string {
+	ss := strings.Split(s, " ")
+	fmt.Println(ss)
+	var res string
+	for i := len(ss) - 1; i >= 0; i-- {
+		res += ss[i]
+		if i != 0 {
+			res += " "
+		}
+	}
+	return res
+}
