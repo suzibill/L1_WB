34ecbb3a0d67f45326adafe6338cf8134e50dbe6
suzibill <suzibill@student.21-school.ru>
2023-03-13 21:49:34 +0300

    ex18

--- ex18/main.go
+++ ex18/main.go
@@ -1,5 +1,42 @@
 package main
 
-  func main() {
+import (
+	"fmt"
+	"sync"
+)
 
-  }
+type Counter struct {
+	count int
+	mu    sync.Mutex
+}
+
+func main() {
+	ch := make(chan int)
+	var c Counter
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go counter(5, ch, &c, &wg)
+
+	for i := 0; i < 100; i++ {
+		ch <- i
+	}
+	close(ch)
+	wg.Wait()
+	fmt.Println(c)
+}
+
+func counter(worker int, ch chan int, c *Counter, wg *sync.WaitGroup) {
+	wg.Add(worker)
+	for i := 0; i < worker; i++ {
+		go func(i int) {
+			for v := range ch {
+				c.mu.Lock()
+				c.count++
+				c.mu.Unlock()
+				fmt.Printf("count: %d worker: %d\n", v, i)
+			}
+			wg.Done()
+		}(i)
+	}
+	wg.Done()
+}
