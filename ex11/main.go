6d85d0c6b9317365250a1647da8d842d544eecc4
suzibill <suzibill@student.21-school.ru>
2023-03-14 19:31:12 +0300

    Update ex11/main.go
    reduce complexity
    
    Co-authored-by: Vladislav Tishchenko <me@xenking.pro>

--- ex11/main.go
+++ ex11/main.go
@@ -6,17 +6,14 @@ func main() {
 	a1 := [10]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 	a2 := [10]int{7, 9, 30, 4, 50, 6, 70, 8, 90, 10}
 	fmt.Println(a1, a2)
-	m := make(map[int]int)
+    var resp []int
+	index := make(map[int]struct{})
 	for _, v := range a1 {
-		m[v] += 1
+		m[v] = struct{}
 	}
 	for _, v := range a2 {
-		m[v] += 1
-	}
-	var res []int
-	for k, v := range m {
-		if v == 2 {
-			res = append(res, k)
+		if _, exists := index[v]; exists {
+			resp = append(resp, v)
 		}
 	}
 	fmt.Println(res)
